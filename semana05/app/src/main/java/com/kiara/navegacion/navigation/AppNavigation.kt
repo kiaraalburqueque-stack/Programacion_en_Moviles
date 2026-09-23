b08f199ca2acc6a8d228ff5765edaa3a3595bb06
kiaraalburqueque-stack <kiara.alburqueque@tecsup.edu.pe>
2026-09-23 03:21:53 -0500

    feat: Screen.kt y AppNavigation.kt con NavHost

---
+++ semana05/app/src/main/java/com/kiara/navegacion/navigation/AppNavigation.kt
@@ -0,0 +1,30 @@
+package com.kiara.navegacion.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.kiara.navegacion.screens.DetailScreen
+import com.kiara.navegacion.screens.HomeScreen
+import com.kiara.navegacion.screens.ListScreen
+import com.kiara.navegacion.screens.ProfileScreen
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Screen.Home.route) {
+        composable(Screen.Home.route) { HomeScreen(navController) }
+        composable(Screen.List.route) { ListScreen(navController) }
+        composable(
+            route = Screen.Detail.route,
+            arguments = listOf(navArgument("itemId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+            DetailScreen(itemId = itemId, navController = navController)
+        }
+        composable(Screen.Profile.route) { ProfileScreen(navController) }
+    }
+}
