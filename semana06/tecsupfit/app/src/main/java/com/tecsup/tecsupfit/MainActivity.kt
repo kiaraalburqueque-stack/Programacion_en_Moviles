b182d95cb96537d3645fe799cc70256cdb6d4968
kiaraalburqueque-stack <kiara.alburqueque@tecsup.edu.pe>
2026-09-23 11:13:06 -0500

    refactor: enlazar MainScreen como punto de entrada en MainActivity

--- semana06/tecsupfit/app/src/main/java/com/tecsup/tecsupfit/MainActivity.kt
+++ semana06/tecsupfit/app/src/main/java/com/tecsup/tecsupfit/MainActivity.kt
@@ -3,45 +3,16 @@ package com.tecsup.tecsupfit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.tecsup.tecsupfit.ui.theme.TecsupfitTheme
+import androidx.compose.material3.MaterialTheme
+import com.tecsup.tecsupfit.ui.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            TecsupfitTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MaterialTheme {
+                MainScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TecsupfitTheme {
-        Greeting("Android")
     }
 }
